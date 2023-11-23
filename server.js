40d3fa5e41d075d313ae8771a61121f163c8b278
Daler Kabilov <d.kabilov@adriver.ru>
2023-11-23 15:41:25 +0300

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const app = express();
 const fs = require('fs');
+const path = require("path");
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
@@ -14,91 +15,60 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/public', express.static('public'));
-
+app.use('/up', express.static('public'));
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/xml'); 
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname); 
-  },
+    const fileName = file.originalname.toLowerCase().split(' ').join('-');
+    cb(null, fileName);
+  }
 });
-const upload = multer({ storage });
 
-const trackingEvents = {
-  acceptInvitation: ["acceptInvitation"],
-  acceptInvitation1: ["acceptInvitation1"],
-  close: ["close"],
-  close1: ["close1"],
-  collapse: ["collapse"],
-  collapse1: ["collapse1"],
-  complete: ["complete"],
-  complete1: ["complete1"],
-  creativeView: ["creativeView"],
-  creativeView1: ["creativeView1"],
-  exitFullscreen: ["exitFullscreen"],
-  exitFullscreen1: ["exitFullscreen1"],
-  expand: ["expand"],
-  expand1: ["expand1"],
-  firstQuartile: ["firstQuartile"],
-  firstQuartile1: ["firstQuartile1"],
-  fullscreen: ["fullscreen"],
-  fullscreen1: ["fullscreen1"],
-  midpoint: ["midpoint"],
-  midpoint1: ["midpoint1"],
-  mute: ["mute"],
-  mute1: ["mute1"],
-  pause: ["pause"],
-  pause1: ["pause1"],
-  progress: ["progress"],
-  progress1: ["progress1"],
-  resume: ["resume"],
-  resume1: ["resume1"],
-  rewind: ["rewind"],
-  rewind1: ["rewind1"],
-  skip: ["skip"],
-  skip1: ["skip1"],
-  start: ["start"],
-  start1: ["start1"],
-  thirdQuartile: ["thirdQuartile"],
-  thirdQuartile1: ["thirdQuartile1"],
-  unmute: ["unmute"],
-  unmute1: ["unmute1"]
-};
+const upload = multer({ storage: storage });
 
-for (const eventName in trackingEvents) {
-  const eventUrl = trackingEvents[eventName];
-  app.get(`/${eventUrl[0]}`, (req, res) => {
-    console.log(`Принят запрос для события ${eventName} по URL: ${eventUrl}`);
-    const filePath = 'public/xml/event.gif';
-  
-    res.sendFile(filePath, { root: __dirname });
-  });
-}
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Внутренняя ошибка сервера');
-});
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.post('/upload', upload.single('file'), (req, res) => {
-  res.status(200).json({ message: 'Файл успешно загружен' });
-});
-
-app.get('/file-list', (req, res) => {
-  const xmlDir = 'public/xml';
-  fs.readdir(xmlDir, (err, files) => {
-    if (err) {
-      console.error('Ошибка чтения папки:', err);
-      res.status(500).json({ error: 'Ошибка чтения папки' });
-    } else {
-      const xmlFiles = files.filter((file) => file.endsWith('.xml'));
-      res.json({ files: xmlFiles });
+  try {
+    if (!req.file) {
+      return res.status(400).send('Выберите файл для загрузки.');
     }
-  });
+    res.status(200).send('Файл успешно загружен!');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
+app.get('/', (req, res) => {
+  res.send("hdasda")
+});
+app.get('/files', (req, res) => {
+  try {
+    const files = fs.readdirSync('uploads/');
+    res.status(200).json(files);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get('/open/:filename', (req, res) => {
+  try {
+    const fileName = req.params.filename;
+    const filePath = path.join(__dirname, 'uploads', fileName);
+
+    // Проверяем, существует ли файл
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).send('Файл не найден.');
+    }
+
+    // Отправляем файл обратно клиенту
+    res.sendFile(filePath);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 const port = 3000;
 app.listen(port, () => {
