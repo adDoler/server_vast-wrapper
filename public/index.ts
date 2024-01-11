3dbf7da192d9fd6c35afeff1380bc231a56d53e1
Daler Kabilov <d.kabilov@adriver.ru>
2024-01-11 15:14:49 +0300

    aab

---
+++ public/index.ts
@@ -0,0 +1,9 @@
+import AdblockDetector from "../src/adblock-detector";
+
+const spanEl = document.getElementById("result");
+
+AdblockDetector.check().then((result) => {
+    spanEl!.textContent = `Блокировщик${result ? "" : " не"} обнаружен`;
+    spanEl!.style.color = result ? "red" : "green";
+});
+alert("hi");
